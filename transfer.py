7987a8334f2ef3a0f03c7cc567f5ec1e1248a33a
Bai_Asuka <sonoda_umi@163.com>
2020-01-07 16:39:31 +0800

    修改更易理解的变量名

--- transfer.py
+++ transfer.py
@@ -4,13 +4,13 @@ from config import *
 
 
 class Transfer:
-    def __init__(self, s_host, s_port=6666):
-        self._server_host = s_host
-        self._server_port = s_port
+    def __init__(self, target_host, target_port=6666):
+        self._target_host = target_host
+        self._target_port = target_port
 
     def msgSender(self, msg):
         s = socket.socket()
-        s.connect((self._server_host, self._server_port))
+        s.connect((self._target_host, self._target_port))
         s.send(bytes(json.dumps(msg), encoding="utf-8")+MESSAGE_OVER_SIGN)
         s.close()
         return True
